a6c99a0c33a98d98a569acdec2a4c7d4fedb7f15
crunai <168853415+crunai@users.noreply.github.com>
2024-05-27 00:29:06 +1000

    add homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { TypewriterEffectSmooth } from "../components/TypeWriterEffect/TypeWriterEffect";
+
+const words = "Plan events with".split(" ").map((w) => {
+  return { text: w };
+});
+const highlightedWord = {
+  text: "Sync.",
+  className: "text-emerald-500 dark:text-emerald-500",
+};
+const renderedWords = words.concat(highlightedWord);
+
+export default function Home() {
+  return (
+    <div className="flex h-[35rem] flex-col items-center justify-center">
+      <p className="text-sm text-neutral-600 sm:text-base xl:text-xl">
+        The Solution for Group Scheduling
+      </p>
+      <TypewriterEffectSmooth words={renderedWords} />
+      <div className="flex flex-col space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+        <Link to="/create">
+          <button className="btn btn-md border bg-emerald-600 text-white sm:btn-lg hover:bg-emerald-700">
+            Create a New Schedule
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
