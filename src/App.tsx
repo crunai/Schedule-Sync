8f984e482c468e73404831920a6607e9793244e9
crunai <168853415+crunai@users.noreply.github.com>
2024-05-28 01:44:54 +1000

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import CalendarScheduler from "./components/CalendarScheduler/CalendarScheduler";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Scaffold from "./pages/Scaffold";
+import { lazy } from "react";
+import Home from "./pages/Home";
+
+const Create = lazy(() => import("./pages/Create"));
+const Schedule = lazy(() => import("./pages/Schedule"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Scaffold />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/create",
+        element: <Create />,
+      },
+      {
+        path: "/schedule/:scheduleId",
+        element: <Schedule />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <main>
-      <CalendarScheduler />
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
